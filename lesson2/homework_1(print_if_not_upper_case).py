a74953dcdd29c6e14bab1c3ee3f9e50bbfb62426
BogdanPan <tru_panku@ukr.net>
2019-06-08 20:46:41 +0300

    name fix and small fix of strn

--- lesson2/homework_1(print_if_not_upper_case).py
+++ lesson2/homework_1(print_if_not_upper_case).py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
-    strn='o hello There'
-    for i in strn:
+    string='o hello There'
+    for i in string:
         if not i.isupper():
             print(i, end='')
         else: break
