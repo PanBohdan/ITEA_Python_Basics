988bca8ce86e297650c9d00a88855cd6ce9a121f
BogdanPan <tru_panku@ukr.net>
2019-06-09 16:54:04 +0300

    Triangle fix
    tringle>triangle

--- lesson2/homework_6(triangle).py
+++ lesson2/homework_6(triangle).py
@@ -1,5 +1,5 @@
 # print triangle with the height of h
-def build_tringle(h):
+def build_triangle(h):
     for i in range(h):
         rows = [(h - i) * ' ' + i * 2 * '^' + '^']
         for i in rows:
@@ -8,4 +8,4 @@ def build_tringle(h):
 
 if __name__ == '__main__':
     h1 = int(input('Input height of triangle '))
-    build_tringle(h1)
+    build_triangle(h1)
