487046de22235414cf2cceace7b172e1b5b113ed
BogdanPan <tru_panku@ukr.net>
2019-06-09 13:24:09 +0300

    updated.

--- lesson2/homework_4(prime_numbers).py
+++ lesson2/homework_4(prime_numbers).py
@@ -3,10 +3,10 @@ if __name__ == '__main__':
     inp = int(input('Enter number:'))
     if inp > 1:
         for i in range(2, inp):
-            if i**(inp-1) % inp == 1:
-                print(inp, ' is a prime number')
+            if inp % i == 0:
+                print(inp, ' is not a prime number')
                 break
         else:
-            print(inp, ' is not a prime number')
+            print(inp, ' is a prime number')
     else:
         print(inp, ' is not a prime number')
