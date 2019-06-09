4eb4bac6dd4bc37173c46f9cfce1ce1419848705
BogdanPan <tru_panku@ukr.net>
2019-06-09 22:13:34 +0300

    Fibonacci number removal of redundant ifs

--- lesson2/Fibonacci_number.py
+++ lesson2/Fibonacci_number.py
@@ -3,13 +3,11 @@ if __name__ == '__main__':
     fib = [0]
     i = 0
     n = int(input('Input how many Fibonacci numbers you want:'))
-    if n == 1:
-        print(fib)
-    else:
+
+    if n > 1:
         fib.append(1)
-    while i <= n-3:
-        i1 = i+1
-        fib.append(int(fib[i]) + int(fib[i1]))
-        i += 1
-    if n != 1:
-        print(fib)
+        while i <= n - 3:
+            i1 = i + 1
+            fib.append(int(fib[i]) + int(fib[i1]))
+            i += 1
+    print(fib)
