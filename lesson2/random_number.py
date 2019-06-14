192ffd3c76e01df8ba42ff9c7c2385688fe9039d
Bogdan Panko <tru_panku@ukr.net>
2019-06-14 21:47:52 +0300

    text fix

--- lesson2/random_number.py
+++ lesson2/random_number.py
@@ -1,9 +1,9 @@
 # print random number in the range that user inputs
 import random
 if __name__ == '__main__':
-    print('Input lowest ran number')
+    print('Input lowest random number')
     inp1 = int(input())
-    print('Input highest ran number')
+    print('Input highest random number')
     inp2 = int(input())
     if inp1 < inp2:
         num = random.randint(inp1, inp2)
