e31b7b9ed51f4332d24c2a969e8b180a8261d9f8
Bogdan Panko <tru_panku@ukr.net>
2019-06-14 21:48:43 +0300

    blank lines after import

--- lesson2/sin(x)mult_by_cos(x).py
+++ lesson2/sin(x)mult_by_cos(x).py
@@ -1,5 +1,7 @@
 # print cos(x) multiplied by sin(x)
 import math
+
+
 if __name__ == '__main__':
     x = float(input())
     print(math.cos(x)*math.sin(x))
--- lesson2/square_root.py
+++ lesson2/square_root.py
@@ -1,5 +1,7 @@
 # print square roots of number until square root is less then 11
 import math
+
+
 if __name__ == '__main__':
     sqr_not_ready = float(input('Enter number '))
     sqr = math.sqrt(sqr_not_ready)
