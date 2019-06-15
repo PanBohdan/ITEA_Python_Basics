d6674d8c85aa06abfeee6eb0284588ee392866fb
Bogdan Panko <tru_panku@ukr.net>
2019-06-15 09:06:53 +0300

    Fix i guess

--- lesson2/homework_4(prime_numbers).py
+++ lesson2/homework_4(prime_numbers).py
@@ -2,14 +2,24 @@
 import math
 
 
-if __name__ == '__main__':
-    inp = int(input('Enter number:'))
-    if inp > 1:
-        for i in range(2, inp):
-            if inp % i == 0:
-                print(inp, ' is not a prime number')
-                break
+def is_number_prime(inp_local):
+    for i in range(int(round(math.sqrt(inp_local))), inp_local-1):
+        if inp % i == 0:
+            return False
         else:
-            print(inp, ' is a prime number')
+            return True
+
+
+if __name__ == '__main__':
+    inp = input('Enter number:')
+    if inp.isdigit():
+        inp = int(inp)
+        if inp > 0:
+            if is_number_prime(inp):
+                print('Number is prime')
+            else:
+                print('Number is not prime')
+        else:
+            print('Error')
     else:
-        print(inp, ' is not a prime number')
+        print('Error')
