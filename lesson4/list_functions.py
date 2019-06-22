197a11ee2e74e9fb35e39296c83918ba53f80058
Bogdan Panko <tru_panku@ukr.net>
2019-06-22 14:37:00 +0300

    simplifying

--- lesson4/list_functions.py
+++ lesson4/list_functions.py
@@ -1,62 +1,15 @@
 def insert(inp_list, inp_index, inp_str):
-    inp_list = inp_list + ['temp']
-    if inp_index >= len(inp_list)-1:
-        inp_list[len(inp_list)-1] = inp_str  # insert at last index of list
-        return inp_list
-
-    if inp_index > -1:  # for positive indexes
-        for i in range(len(inp_list)-1):
-            if i == inp_index:  # inserting input_str
-                temp = inp_list[i]
-                inp_list[i] = inp_str
-            if i > inp_index:  # moving all of the other objects in the list
-                temp_temp = temp
-                temp = inp_list[i]
-                inp_list[i] = temp_temp
-        temp_temp = temp
-        inp_list[i+1] = temp_temp
-        return inp_list
-
-    # for negative indexes that larger then length of input list
-    if -inp_index >= len(inp_list)-1:
-        # moving back in cycle of length of input list
-        for i in range(len(inp_list)-1):
-            if i == 0:  # inserting at the index of beginning
-                temp = inp_list[i]
-                inp_list[i] = inp_str
-            if i > 0:
-                temp_temp = temp  # moving all of objects by one index
-                temp = inp_list[i]
-                inp_list[i] = temp_temp
-        temp_temp = temp
-        inp_list[i+1] = temp_temp
-        return inp_list
-
-    if inp_index <= -1:  # for negative indexes
-        for i in range(len(inp_list)-1):
-            if i == inp_index+len(inp_list)-1:  # inserting at the index
-                temp = inp_list[i]
-                inp_list[i] = inp_str
-            if i > inp_index+len(inp_list)-1:  # moving objects
-                temp_temp = temp
-                temp = inp_list[i]
-                inp_list[i] = temp_temp
-        temp_temp = temp
-        inp_list[i+1] = temp_temp
-        return inp_list
+    new_list = inp_list[:inp_index] + [inp_str] + inp_list[inp_index:]
+    return new_list
 
 
 def remove(inp_list, inp_str):
     index = 0
-    first_run = True
-    new_list = []
     for i in inp_list:
-        if i == inp_str and first_run:  # removing
-            inp_list[index] = ''
-            first_run = False
-            continue
-        new_list = new_list + [i]  # moving along
+        if i == inp_str:
+            break
         index += 1
+    new_list = inp_list[:index] + inp_list[index+1:]
     return new_list
 
 
