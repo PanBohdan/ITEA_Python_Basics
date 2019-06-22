7632e852d3ae66af89e1093f1a5aa5a49bce1c42
Bogdan Panko <tru_panku@ukr.net>
2019-06-22 13:33:42 +0300

    return and input fix

--- lesson2/filing_list_with_random_numbers.py
+++ lesson2/filing_list_with_random_numbers.py
@@ -1,16 +1,15 @@
 import random
 
 
-def create_list_with_random_numbers(start, end):
+def create_list_with_random_numbers(start, end, num_of_numbers):
     list1 = []
-    num_of_numbers = int(input('Input how many random numbers you want:'))
     if num_of_numbers >= 1:
         for i in range(num_of_numbers):
             list1.append(random.randint(start, end))
         return list1
     else:
-        print('You entered negative number or zero. '
-              'Please try again but with positive number')
+        return ('You entered negative number or zero. '
+                'Please try again but with positive number')
 
 
 if __name__ == "__main__":
@@ -19,6 +18,7 @@ if __name__ == "__main__":
     print('Input highest ran number')
     inp_max = int(input())
     if inp_min <= inp_max:
-        print(create_list_with_random_numbers(inp_min, inp_max))
+        n = int(input('Input how many random numbers you want:'))
+        print(create_list_with_random_numbers(inp_min, inp_max, n))
     else:
         print('Error: first number is larger than second')
